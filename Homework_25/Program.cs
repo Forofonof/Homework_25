2b92f99508a6d5080c19ae7c8020d574e588ee36
Вячеслав Форофонов <DanielResult1@outlook.com>
2022-09-09 16:44:24 +0300

    Добавьте файлы проекта.

---
+++ Homework_25/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        string text = "Я точно стану Junior Unity Developers.";
+        string[] newLine = text.Split(' ');
+
+        foreach (var word in newLine)
+        {
+            Console.WriteLine($"{word}");
+        }
+    }
+}
